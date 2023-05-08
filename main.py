dcd530ed6b45b9117baa0ebb44a4fb085176a082
Azerty2285 <129675356+Azerty2285@users.noreply.github.com>
2023-05-08 17:14:50 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ player_image = image.load("IMAGES/carplayer1234.png")
 enemy1_image = image.load("IMAGES/car123.png")
 enemy2_image = image.load("IMAGES/car12345.png")
 enemy3_image = image.load("IMAGES/car1235678.png")
+tree_image = image.load("IMAGES/tree.png")
 #enemy_image = image.load("")
 
 # фонова музика
@@ -29,7 +30,11 @@ enemy3_image = image.load("IMAGES/car1235678.png")
 #mixer.music.set_volume(0.2)
 #mixer.music.play(-1)
 # класи
-time
+path =os.getcwd()
+exp_images = os.listdir(path+"/explotion")
+images_list = []
+#for img in exp_images:
+    #images_list.append(transform.scale(image.load("explotion/"+img), (150, 150)))
 
 
 class GameSprite(sprite.Sprite):
@@ -50,9 +55,9 @@ class Player(GameSprite):
         global bg_speed
         global max_speed
         keys_pressed = key.get_pressed() 
-        if keys_pressed[K_LEFT] and self.rect.x > 0:
+        if keys_pressed[K_LEFT] and self.rect.x > 280:
             self.rect.x -= self.speed
-        if keys_pressed[K_RIGHT] and self.rect.x < WIDTH - 70:
+        if keys_pressed[K_RIGHT] and self.rect.right < 905:
             self.rect.x += self.speed
         if keys_pressed[K_UP] and bg_speed<max_speed:
             bg_speed = bg_speed+0.3
@@ -86,6 +91,34 @@ class Enemy(GameSprite):
             if collide!= self:
                 self.get_random_pos()
 
+class Tree(GameSprite):
+    def update(self):
+        if self.rect.y < HEIGHT:
+            self.rect.y+=self.speed
+        else:
+            self.get_random_pos1()
+
+
+
+    def get_random_pos1(self):
+        rand_num = randint(1, 15)
+        if rand_num == 1:
+            self.rect.x = 280
+            self.rect.y = -300
+        if rand_num == 2:
+            self.rect.x = 445
+            self.rect.y = -500
+        if rand_num == 3:
+            self.rect.x = 630
+            self.rect.y = -700
+        if rand_num == 4:
+            self.rect.x = 805
+            self.rect.y = -150
+        collide_list = sprite.spritecollide(self, trees, False)
+        for collide in collide_list:
+            if collide!= self:
+                self.get_random_pos1()
+
         
 
 class Text(sprite.Sprite):
@@ -112,13 +145,13 @@ display.set_caption("CAR RACES")
 # написи для лічильників очок
 score_text = Text("Рахунок: 0", 20, 50)
 # напис з результатом гри 
-result_text = Text("LOSE!", 350, 250, font_size = 50)
-
+result_text = Text("LOSE!", 360, 250, font_size = 150)
 #додавання фону
 bg = transform.scale(bg1, (WIDTH, HEIGHT))
 
 # створення спрайтів
 player = Player(player_image, width = 100, height = 200, x = 500, y = 500)
+tree = GameSprite(tree_image, width = 100, height = 200, x = 400, y = 100)
 #enemy2 = GameSprite(enemy2_image, width = 100, height = 200, x = 400, y = 100)
 # основні змінні для гри
 run = True
@@ -128,7 +161,7 @@ FPS = 60
 score = 0
 lost = 0
 enemyies = sprite.Group()
-
+trees = sprite.Group()
 
 for i in range(1):
     rand_y = randint(-500, -100)
@@ -141,6 +174,13 @@ for i in range(1):
     enemy3= Enemy(enemy3_image , width= 100, height=200, y = rand_y, x = rand_x, speed = rand_speed)
     enemy3.get_random_pos()
     enemyies.add(enemy1, enemy2, enemy3)    
+
+for i in range(1):
+    rand_y = randint(-500, -100)
+    rand_x = randint(200, 1000)
+    rand_speed = randint(3, 5)
+    tree= Tree(tree_image , width= 100, height=200, y = rand_y, x = rand_x, speed = rand_speed)
+    tree.get_random_pos1() 
 # ігровий цикл
 
 time_text = Text("Time:", WIDTH - 150, 25, font_size = 40)
@@ -159,9 +199,9 @@ while run:
         time_text.set_text("Time:" + str(time_left))
         distance_text.set_text("Distance: " + str(distance))
         if time_left <= 0:
+            result_text.set_text("LOSE!!!")
             finish = True
-        window.blit(bg, (0, bg1_y))
-        window.blit(bg2, (0, bg2_y))
+        
         bg1_y +=bg_speed
         bg2_y +=bg_speed
         if bg1_y > 700:
@@ -170,19 +210,29 @@ while run:
             bg2_y = -700
         if bg_speed > 7:
             bg_speed -= 0.1
-        player.draw()
-        time_text.draw()  
-        enemyies.draw(window)
-        #enemy2.draw()
+       
         player.update() #рух гравця
         enemyies.update() #ру
+        trees.update()
         distance -= int(bg_speed*0.40)
-        distance_text.draw()  
+        
         if distance <= 0:
+            result_text.set_text("WIN!!!")
             finish = True
-        spritelist = sprite.spritecollide(player, enemyies, False, sprite.collide_mask)
+        spritelist = sprite.spritecollide(player, enemyies, False, sprite.collide_mask)   
         for collide in spritelist:
+            #explosions.add(Explosion(collide.rect.x, collide.rect.y, images_list))
             result_text.set_text("LOSE!!!")
-            finish = True
+            finish = True 
+    window.blit(bg, (0, bg1_y))
+    window.blit(bg2, (0, bg2_y))
+    player.draw()
+    trees.draw()
+    time_text.draw()  
+    enemyies.draw(window)
+    distance_text.draw()  
+    if finish:
+        result_text.draw()
+
     display.update()
     clock.tick(FPS)
